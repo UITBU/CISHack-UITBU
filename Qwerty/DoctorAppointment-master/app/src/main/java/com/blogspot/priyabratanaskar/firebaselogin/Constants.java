d2964c827d335c48b5d649feed76221d20833055
Md. Nawaz Rahaman <53112938+Nawaz2000@users.noreply.github.com>
2021-04-08 06:50:04 +0530

    Project directory added

---
+++ Qwerty/DoctorAppointment-master/app/src/main/java/com/blogspot/priyabratanaskar/firebaselogin/Constants.java
@@ -0,0 +1,15 @@
+package com.blogspot.priyabratanaskar.firebaselogin;
+
+/**
+ * Created by Priyabrata Naskar on 06-04-2021.
+ */
+public class Constants {
+    public static final String doctorName = "name";
+    public static String imageResource = "image";
+    public static String doctorExperience = "experience";
+    public static String doctorQualification = "qualification";
+    public static String tag ="tag";
+    public static String position ="position";
+    public static String fees ="fees";
+    public static String about ="about";
+}
